bf14657bc879153c26448a3089b7b3b73e74280e
simpaul <28johnpaulsim28@gmail.com>
2021-03-29 09:13:17 +0800

    add resource route

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::get('products', [ProductsController::class, 'index']);
+Route::resource('products', ProductsController::class);
